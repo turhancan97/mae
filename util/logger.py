1df42901f30ec4cf30b6bb3c447453c8ff5ca829
Turhan Can Kargın <turhancan.kargin@gmail.com>
2025-02-19 10:28:37 +0100

    Add WandbLogger utility for experiment tracking

---
+++ util/logger.py
@@ -0,0 +1,35 @@
+import wandb
+import torch
+
+class WandbLogger(object):
+    def __init__(self, args):
+        wandb.init(
+            config=args,
+            entity=args.wandb_entity,
+            project=args.wandb_project,
+            group=getattr(args, 'wandb_group', None),
+            name=getattr(args, 'wandb_run_name', None)
+        )
+
+    def set_step(self, step=None):
+        if step is not None:
+            self.step = step
+        else:
+            self.step += 1
+
+    def update(self, metrics):
+        log_dict = dict()
+        for k, v in metrics.items():
+            if v is None:
+                continue
+            if isinstance(v, torch.Tensor):
+                v = v.item()
+            log_dict[k] = v
+
+        wandb.log(log_dict, step=self.step)
+    
+    def log_image(self, image, name):
+        wandb.log({name: wandb.Image(image, caption=name)}, step=self.step)
+
+    def flush(self):
+        pass

bd8356cd81fb9976d4e64f9bef3906872eda4614
Turhan Can Kargın <turhancan.kargin@gmail.com>
2025-03-12 12:16:57 +0100

    Update checkpoint naming to use SiamMAE prefix
    - Modify save_model function to use 'SiamMAE-checkpoint' naming convention
    - Update checkpoint path generation for both loss scaler and model save methods

--- util/misc.py
+++ util/misc.py
@@ -296,7 +296,7 @@ def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler):
     output_dir = Path(args.output_dir)
     epoch_name = str(epoch)
     if loss_scaler is not None:
-        checkpoint_paths = [output_dir / ('checkpoint-%s.pth' % epoch_name)]
+        checkpoint_paths = [output_dir / ('SiamMAE-checkpoint-%s.pth' % epoch_name)]
         for checkpoint_path in checkpoint_paths:
             to_save = {
                 'model': model_without_ddp.state_dict(),
@@ -309,7 +309,7 @@ def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler):
             save_on_master(to_save, checkpoint_path)
     else:
         client_state = {'epoch': epoch}
-        model.save_checkpoint(save_dir=args.output_dir, tag="checkpoint-%s" % epoch_name, client_state=client_state)
+        model.save_checkpoint(save_dir=args.output_dir, tag="SiamMAE-checkpoint-%s" % epoch_name, client_state=client_state)
 
 
 def convert_qkv_weights(state_dict):
